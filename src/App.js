83b483fff6d1f94f735061a25800105835a9ce27
TheodorCristian <theodorc.petre@gmail.com>
2022-10-12 12:37:33 +0300

    minor update on app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import { AuthProvider } from "./Contexts/AuthContext";
 import Signup from "./Components/User/Pages/Signup/Signup";
 import Login from "./Components/User/Pages/LogIn/Login";
@@ -26,28 +21,22 @@ const App = () => {
     <div className="App">
       <AuthProvider>
         <Router>
-          <Switch>
-            <Redirect exact from="/" to="/login" />
-            <Route path="/signup" exact component={Signup} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/add-recipe" exact component={AddRecipe} />
-            <Route path="/add-category" exact component={AddCategory} />
-            <Route path="/add-ingredient" exact component={AddIngredient} />
-            <Route path="/edit-recipe/:id" exact component={EditRecipe} />
-            <Route path="/edit-category/:id" exact component={EditCategory} />
-            <Route
-              path="/edit-ingredient/:id"
-              exact
-              component={EditIngredient}
-            />
-            <Route path="/profile" exact component={UserProfile} />
-            <Route path="/home" exact component={Home} />
-            <Layout>
-              <Route path="/recipes/:id" exact component={CategoryPage} />
-              <Route path="/recipes/:cat/:id" exact component={RecipePage} />
-            </Layout>
-          </Switch>
+          <Redirect exact from="/" to="/login" />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/add-recipe" exact component={AddRecipe} />
+          <Route path="/add-category" exact component={AddCategory} />
+          <Route path="/add-ingredient" exact component={AddIngredient} />
+          <Route path="/edit-recipe/:id" exact component={EditRecipe} />
+          <Route path="/edit-category/:id" exact component={EditCategory} />
+          <Route path="/edit-ingredient/:id" exact component={EditIngredient} />
+          <Route path="/profile" exact component={UserProfile} />
+          <Route path="/home" exact component={Home} />
+          <Layout>
+            <Route path="/recipes/:id" exact component={CategoryPage} />
+            <Route path="/recipes/:cat/:id" exact component={RecipePage} />
+          </Layout>
         </Router>
       </AuthProvider>
     </div>
