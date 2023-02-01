9187622d50e36a8ed705c9a80d182bea8c038dd5
TheodorCristian <theodorc.petre@gmail.com>
2023-02-01 18:16:02 +0200

    v.1.1 restyle on profile icon menu and new dependency added -material ui-

--- src/Components/User/ProfileNavbarIcon/ProfileNavbarIcon.js
+++ src/Components/User/ProfileNavbarIcon/ProfileNavbarIcon.js
@@ -1,15 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./ProfileNavbarIcon.scss";
-import ProfileIcon from "../../../Assets/images/profile.png";
-import { Link } from "react-router-dom";
+import { Button, Menu, MenuItem, Divider } from "@material-ui/core";
 
 function ProfileNavbarIcon() {
+  let user = JSON.parse(sessionStorage.getItem("user")).data;
+  let userName = user.first_name;
+  let userAvatar = user.avatar;
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const goToProfile = () => {
+    navigate("/profile");
+  };
+
+  const goToWishlist = () => {
+    navigate("/wishlist");
+  };
+
+  const logout = async () => {
+    try {
+      await logout();
+      sessionStorage.setItem("user", null);
+      sessionStorage.setItem("systemUserId", null);
+      sessionStorage.setItem("isAdmin", null);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className="profile__icon">
-      <Link to="/profile">
-        <img src={ProfileIcon} alt="Profile Icon" />
-      </Link>
+    <div>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? "demo-positioned-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <div className="menu__opener">
+          <img src={userAvatar} alt="Avatar"></img>
+          <p>Hello, {userName}</p>
+        </div>
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <MenuItem onClick={goToProfile}>MY PROFILE</MenuItem>
+        <MenuItem onClick={goToWishlist}>WISHSLIST</MenuItem>
+        {/*<MenuItem onClick={handleClose}>SHOPPING LIST</MenuItem>*/}
+        <Divider />
+        <MenuItem onClick={logout}>LOG OUT</MenuItem>
+      </Menu>
     </div>
   );
 }
